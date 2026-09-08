711e31251283310c28edfafdb40ed9e545d9333d
Tanner Davison <tanner.davison95@gmail.com>
2026-09-08 00:44:59 -0600

    save dcmake

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -6,6 +6,44 @@
 int main(int argc, char** argv) {
     static constexpr int window_width{800};
     constexpr int        window_height{600};
+
+    tf::Taskflow taskflow;
+
+    int i;
+
+    auto [init, body, cond, done] = taskflow.emplace(
+        [&]() {
+            std::cout << "i=0\n";
+            i = 0;
+        },
+        [&]() {
+            std::cout << "i++ => i=";
+            i++;
+        },
+        [&]() {
+            std::cout << i << '\n';
+            return i < 5 ? 0 : 1;
+        },
+        [&]() {
+            std::cout << "done\n";
+        });
+
+    init.name("init");
+    body.name("do i++");
+    cond.name("while i<5");
+    done.name("done");
+
+    init.precede(body);
+    body.precede(cond);
+    cond.precede(body, done);
+
+    // taskflow.dump(std::cout);
+
+    std::ofstream os(".cache/taskflow.dot");
+    taskflow.dump(os);
+    tf::Executor executor;
+    executor.run(taskflow).wait();
+
     try {
         App app(window_width, window_height, "Tannery");
         app.run();

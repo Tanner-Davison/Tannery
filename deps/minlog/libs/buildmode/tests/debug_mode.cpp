a5b98c6f88ad77beae762d07f261fac4b4941a44
Tanner Davison <tanner.davison95@gmail.com>
2026-09-09 01:22:07 -0600

    Convert deps/minlog and deps/taskflow to proper git submodules; clean up stale .gitmodules entry

--- deps/minlog/libs/buildmode/tests/debug_mode.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <buildmode.hpp>
-
-int main ()
-{
-	if constexpr (BuildMode::isDebug && !BuildMode::isRelease)
-		return EXIT_SUCCESS;
-	return EXIT_FAILURE;
-}
--- deps/minlog/libs/buildmode/tests/release_mode.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <buildmode.hpp>
-
-int main ()
-{
-	if constexpr (BuildMode::isRelease && !BuildMode::isDebug)
-		return EXIT_SUCCESS;
-	return EXIT_FAILURE;
-}

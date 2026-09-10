3493de78894ef58359f92adce0d608b0790f1c58
Tanner Davison <tanner.davison95@gmail.com>
2026-09-10 00:27:10 -0600

    removed bad include Directive placement

--- core/include/shaderModule.hpp
+++ core/include/shaderModule.hpp
@@ -6,4 +6,3 @@
 std::vector<char> readFile(const std::filesystem::path& filename);
 
 VkShaderModule createShaderModule(VkDevice device, const std::vector<char>& code);
-#include <vulkan/vulkan_core.h>
